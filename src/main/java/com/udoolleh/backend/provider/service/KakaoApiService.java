30729f52bab7303b9b56c9a7b2b2cb415a2f491d
skfkgla <narahim.lee@gmail.com>
2022-09-16 00:36:05 +0900

    refactor : controller와 service 수정

--- src/main/java/com/udoolleh/backend/provider/service/KakaoApiService.java
+++ src/main/java/com/udoolleh/backend/provider/service/KakaoApiService.java
@@ -91,8 +91,6 @@ public class KakaoApiService implements KakaoApiServiceInterface {
         catch(ParseException e){
             e.printStackTrace();
         }
-
         return (boolean)jsonMeta.get("is_end");
-
     }
 }
