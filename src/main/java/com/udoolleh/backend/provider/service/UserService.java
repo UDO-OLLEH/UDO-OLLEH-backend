6d2a3714dcf57d1e22b1a0a4fb4813fb705f6727
skfkgla <narahim.lee@gmail.com>
2022-10-04 14:35:27 +0900

    refactor: 예외처리 수정

--- src/main/java/com/udoolleh/backend/provider/service/UserService.java
+++ src/main/java/com/udoolleh/backend/provider/service/UserService.java
@@ -90,6 +90,9 @@ public class UserService implements UserServiceInterface {
     @Override
     public void logout(String email) {
         User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new CustomJwtRuntimeException();
+        }
 
         user.changeRefreshToken("");
     }
