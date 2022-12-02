94171756f1f0a150e9f370d3683ef532d9c86b9d
alivejuicy <kkddhh1998@gmail.com>
2022-12-02 14:36:12 +0900

    Feat(Ads) : 광고 사진 Entity 생성

---
+++ src/main/java/com/udoolleh/backend/entity/Ads.java
@@ -0,0 +1,30 @@
+package com.udoolleh.backend.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Table(name = "ads")
+@NoArgsConstructor
+@Getter
+@Entity
+public class Ads {
+    @Id
+    @Column(name = "ads_id")
+    private String id = UUID.randomUUID().toString();
+
+    @Column(name = "photo")
+    private String photo;
+
+    @Builder
+    public Ads(String id, String photo) {
+        this.id = id;
+        this.photo = photo;
+    }
+}
