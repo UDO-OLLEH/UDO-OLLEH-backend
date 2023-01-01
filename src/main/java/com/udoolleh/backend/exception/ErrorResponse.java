9e09ad4f5087f18754a39413b9d3da8abc7cc2ba
Koonayoung518 <koony000415@gmail.com>
2023-01-01 21:33:56 +0900

    refactor: 광고 관련 exception 수정

--- src/main/java/com/udoolleh/backend/exception/ErrorResponse.java
+++ src/main/java/com/udoolleh/backend/exception/ErrorResponse.java
@@ -3,11 +3,22 @@ package com.udoolleh.backend.exception;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.ResponseEntity;
 
 @Builder
 @Getter
 public class ErrorResponse {
     private String code;
     private String message;
-    private int status;
+    private String status;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.builder()
+                        .status(errorCode.getStatus().name())
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build());
+    }
 }
